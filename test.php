cd77c8d4b8055fd468bcda9a7d76ea9f2779ba8c
5thdeveloper <joseph.alonzi@5thbusiness.com>
2014-09-19 02:01:37 -0400

    Quick Class
    This was a quick hello world class created.

--- test.php
+++ test.php
@@ -1,8 +1,17 @@
 <?php
 
-	$message = ('hello world');
+class hello_world{
+	public function say_hello($name){
+			return $this->build_sentence($name);
+	}
+	private function build_sentence($name){
+			$str = 'Hello '.$name.', welcome.'
+			return $str;
+	}
+}
 
-	echo($message);
-	
-	
+$hello = new hello_world();
+
+echo $hello->say_hello('Joe');
+
 ?>
